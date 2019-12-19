42e1ac1f7a24f20de1a6f7b666f7c629dd9a6ce7
Tramble Tomerlin <tramble.tomerlin@gmail.com>
2019-12-19 13:39:09 -0500

    first commit

---
+++ GetIT/src/main/java/com/caren/stuff/GetItApplication.java
@@ -0,0 +1,13 @@
+package com.caren.stuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetItApplication.class, args);
+	}
+
+}
---
+++ GetIT/src/main/java/com/caren/stuff/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.caren.stuff;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GetItApplication.class);
+	}
+
+}
